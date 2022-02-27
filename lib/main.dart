e572afd0fddfdf66f7214b546a96dc851a5eeeb7
devnons <unimkeadie@gmail.com>
2022-02-27 19:31:09 +0100

    centralised app scores at bottom of the screen.

--- lib/main.dart
+++ lib/main.dart
@@ -126,6 +126,7 @@ class _QuizPageState extends State<QuizPage> {
           ),
         ),
         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: scoreKeeper,
         )
       ],
